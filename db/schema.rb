c284f83b7d0cfc782df15764441d105a375ef7d3
Tylere3131 <tyevans3131@gmail.com>
2020-02-26 02:24:57 -0600

    Went through all Views and fixed Forms by getting rid of IDs and replacing them with relevant Names, Customer View is Commented to show only relevant Data. Added pet_name Column to Pet because it was missing.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_185545) do
+ActiveRecord::Schema.define(version: 2020_02_26_063419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -152,6 +152,7 @@ ActiveRecord::Schema.define(version: 2020_02_24_185545) do
     t.decimal "pet_weight"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "pet_name"
     t.index ["customer_id"], name: "index_pets_on_customer_id"
   end
 
