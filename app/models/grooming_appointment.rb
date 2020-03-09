810d6c8cc72ec68e1be862db7d726013c3f5249f
Valerie Smith <valerie@Valeries-MacBook-Pro.local>
2020-03-09 01:59:07 -0500

    Style Nested form works in grooming appt

--- app/models/grooming_appointment.rb
+++ app/models/grooming_appointment.rb
@@ -2,6 +2,7 @@ class GroomingAppointment < ApplicationRecord
   belongs_to :pet
   belongs_to :store
   belongs_to :payment_type
+  has_many :grooming_styles
   has_many :styles, through: :grooming_styles
   has_many :grooming_services
   has_many :services, through: :grooming_services
@@ -11,5 +12,8 @@ class GroomingAppointment < ApplicationRecord
 
   accepts_nested_attributes_for :grooming_services, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :services, reject_if: :all_blank, allow_destroy: true
+
+  accepts_nested_attributes_for :grooming_styles, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :styles, reject_if: :all_blank, allow_destroy: true
   
 end
--- app/models/style.rb
+++ app/models/style.rb
@@ -1,3 +1,4 @@
 class Style < ApplicationRecord
     has_many :grooming_styles
+    has_many :grooming_appointments, through: :grooming_styles
 end
