d125a70d2fd3bd6b1ab9947766586e081ef15564
Tylere3131 <tyevans3131@gmail.com>
2020-03-18 21:19:00 -0500

    Created table styling in the show view, styled the forms in the edit/new view, added nested Add Store form to New Employee view

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,10 +1,12 @@
 class Employee < ApplicationRecord
   belongs_to :state
+  has_many :employee_stores
   has_many :stores, through: :employee_stores
-
+  
   validates :emp_first_name, presence: true
   validates :emp_last_name, presence: true
   validates :emp_primary_phone, presence: true
-  
 
+  accepts_nested_attributes_for :stores, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :employee_stores, reject_if: :all_blank, allow_destroy: true
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,8 @@
 class Store < ApplicationRecord
   belongs_to :state
+  
   has_many :employee_stores
   has_many :grooming_appointments
+
+  accepts_nested_attributes_for :employee_stores
 end
