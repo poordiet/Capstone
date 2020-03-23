e7aabccc41ebfe01ef23bebb169e12c8a54ca9eb
Kyle <fullerk89@gmail.com>
2020-03-23 09:08:50 -0500

    removed unnecessary lines of code from customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,12 +2,10 @@ class Customer < ApplicationRecord
   belongs_to :state
   belongs_to :customer_status
   has_many :pets, inverse_of: :customer
-  #has_many :vaccines, through: :pets
-  #has_many :pet_vaccinations, through: :pets
+  
   
 
   accepts_nested_attributes_for :pets, reject_if: :all_blank, allow_destroy: true
-  #accepts_nested_attributes_for :vaccines, reject_if: :all_blank, allow_destroy: true
-  #accepts_nested_attributes_for :pet_vaccinations, reject_if: :all_blank, allow_destroy: true
+  
   
 end
