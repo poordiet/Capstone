12092fec1ac76772b01c5a7ed74f4a14649d7ba3
Valerie Smith <valerie@Valeries-MacBook-Pro.local>
2020-03-30 02:33:06 -0500

    Fixed paramaters so they would stay on the grooming photo creation page

--- app/models/pet_photo.rb
+++ app/models/pet_photo.rb
@@ -3,4 +3,14 @@ class PetPhoto < ApplicationRecord
   belongs_to :photo_type
   belongs_to :grooming_appointment, optional: true
   has_one_attached :image
+
+  validate :photo_attached
+
+  def photo_attached
+    if !self.image.attached?
+      errors[:base] << 'You must attach a photo'
+    end  
+  end
+
+
 end
