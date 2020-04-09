94767a63146e5eb8e0f024d702e147419e28a4d7
Valerie Smith <valerie@Valeries-MacBook-Pro.local>
2020-04-08 23:20:31 -0500

    added photo validations

--- app/models/pet_photo.rb
+++ app/models/pet_photo.rb
@@ -5,10 +5,24 @@ class PetPhoto < ApplicationRecord
   has_one_attached :image
 
   validate :photo_attached
+  validate :attachment_is_photo
+  validate :attachment_is_too_big
 
   def photo_attached
     if !self.image.attached?
       errors[:base] << 'You must attach a photo'
+    end  
+  end
+
+  def attachment_is_too_big
+    if self.image.attached? && image.byte_size > 10000000
+      errors[:base] << 'Image cannot be larger than 10 MB'
+    end  
+  end
+
+  def attachment_is_photo
+    if self.image.attached? && !image.content_type.in?(%w(image/jpeg image/png))
+      errors[:base] << 'Image must be a JPEG or PNG'
     end  
   end
 
